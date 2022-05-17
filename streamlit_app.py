d20d5ecb5aeb9302f60854abc5ce15e445e5946f
Nitish Kumar <105282432+nitgit07@users.noreply.github.com>
2022-05-17 23:59:18 +0530

    Update streamlit_app.py
    enter the fruit name user

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,9 +19,12 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display fruityvice API response
 streamlit.header('Fruityvice Fruit Advice!')
+#Add atextbox to enter fruit name
+fruit_choice=streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered', fruit_choice)
 
 import requests
-fruityvice_response= requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
+fruityvice_response= requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 #output it to the screen as a table
 streamlit.dataframe(fruityvice_normalized)
