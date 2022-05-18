5c63321d93b18419f225f2511a427886711e6e54
Nitish Kumar <105282432+nitgit07@users.noreply.github.com>
2022-05-18 14:54:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,25 +3,24 @@ import pandas
 import requests
 import snowflake.connector
 from urllib.error import URLError
-
 streamlit.title('My parents new healthy diner.')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
 #let's put a pick list here so they can chose the fruit they want to include
 fruits_selected = streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index))
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
 #Display the table on the page
 streamlit.dataframe(fruits_to_show)
-
+#create the repeatable code block called a function
+def get_fruityvice_data(this_fruit_choice)
+  fruityvice_response= requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
 #New section to display fruityvice API response
 streamlit.header('Fruityvice Fruit Advice!')
 #Add atextbox to enter fruit name
@@ -30,10 +29,8 @@ try:
   if not fruit_choice:
     streamlit.error('Please select a fruit to get information.')
   else:
-    fruityvice_response= requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
-    #output it to the screen as a table
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_function=get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
 streamlit.stop()
@@ -43,10 +40,7 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.header("Fruit Load List Contains:")
-
-
 streamlit.dataframe(my_data_row)
-
 #Allow the enduser to add a fruit
 add_my_fruit=streamlit.text_input('What fruit would you like to add?', 'Kiwi')
 streamlit.write('Thanks for adding', add_my_fruit)
